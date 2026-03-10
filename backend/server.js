670cc82e0556cb9f84b8500586a4d867bb023b37
Amna-software-engineer <amnahaqamna83@gmail.com>
2026-03-10 15:40:32 +0500

    updated to deploy

--- backend/server.js
+++ backend/server.js
@@ -15,43 +15,50 @@ const path = require("path");
 
 const app = express();
 const PORT = 3000;
-dotenv.config() 
-const DB_URL=process.env.MONGODB_URL
-const rootDir =path.dirname(require.main.filename)
+dotenv.config()
+const DB_URL = process.env.MONGODB_URL;
+
+const rootDir = path.dirname(require.main.filename)
 
 const storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"./uploads")
+    destination: (req, file, cb) => {
+        cb(null, "./uploads")
     },
-    filename:(req,file,cb)=>{
-        cb(null,Date.now()+"-"+file.originalname)
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname)
     }
 })
-const uplaod = multer({storage})
+const uplaod = multer({ storage })
 app.use(express.static(path.join(rootDir, 'uploads')))
-app.use("/uploads",express.static(path.join(rootDir, 'uploads')))
+app.use("/uploads", express.static(path.join(rootDir, 'uploads')))
 app.use(express.urlencoded({ extended: true }));// to parse form data
 app.use(express.json())
 app.use(cors({
-  origin: "http://localhost:5173",  //  Frontend URL
-  credentials: true                 //  Required for cookies
+    origin: "http://localhost:5173",  //  Frontend URL
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true                 //  Required for cookies
 }));
 
-app.use("/admin/blogs",uplaod.single("image"));
+app.use("/admin/blogs", uplaod.single("image"));
 app.use(authRouter);
 app.use(userRouter);
-app.use("/admin",adminRouter);
+app.use("/admin", adminRouter);
 
 
+const ConnectDb = async () => {
+    try {
+        const connection = await mongoose.connect(
+            DB_URL,
+        );
 
-mongoose.connect(DB_URL).then(()=>{
-    console.log("connected to mongodb successfully ");
-    
-     app.listen(PORT, () => {
-        console.log(`server running at PORT ${PORT}`);
-    })
-}).catch(err=>{
-    console.log("err while connecting to db ",err);
-    
-})
+        if (connection.connection) {
+            console.log("Database Connected Successfully!");
+        }
+    } catch (error) {
+        console.log("Something went wronge while connecting DB");
+    }
+};
+await ConnectDb();
+export default app;
+
 
